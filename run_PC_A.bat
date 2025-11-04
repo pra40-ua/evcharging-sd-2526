dfc1f8aef5633a12a22d7ebda0e4d0faac3618ed
lir5-ua <lir5@alu.ua.es>
2025-11-04 18:44:19 +0100

    cambios

--- run_PC_A.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-REM Abre una nueva ventana de PowerShell y ejecuta todos los comandos de PC_A
-start "Central-PC_A" powershell -NoLogo -NoExit -ExecutionPolicy Bypass -File "%~dp0commands_PC_A.ps1"
